0e4b76b3d30f80d0bd2aef6f99da04befc3f055e
Ari Kai Wynder <arikwynder.work@protonmail.com>
2025-04-29 08:06:36 -0600

    Extract rank and date from Hacker News posts
    Added logic to retrieve and map rank to post date on Hacker News' "newest" page using a Map object. This includes fetching specific elements for rank and date, extracting relevant data, and printing the result. These changes enhance data extraction capabilities for further processing.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,39 @@ async function sortHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
+  let dateMap = new Map();
+
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  // wait for element with class 'rank' to be visible
+  const rankElement = await page.waitForSelector('.rank');
+
+  // get the text content
+  let rank = await rankElement.textContent();
+  rank = parseInt(rank.substring(0, rank.length - 1));
+
+  // // print the rank
+  // console.log('Rank value:', rank);
+
+  // wait for element with class 'age' to be visible
+  const dateElement = await page.waitForSelector('.age');
+
+  //grab the title attribute with the date
+  const date = await dateElement.getAttribute('title');
+
+  // // print the date
+  // console.log('Date value:', date);
+
+  dateMap.set(rank,date);
+
+
+  //print key-value pair
+  console.log('Rank ' + dateMap.keys().next().value + ': ', dateMap.get(dateMap.keys().next().value));
+
+  // close browser
+  await browser.close();
+
 }
 
 (async () => {
